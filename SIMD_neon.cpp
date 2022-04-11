6cf7ad74b79f940f508177c4164583a571d0429a
JXhan <88481593+hanmaxmax@users.noreply.github.com>
2022-04-11 20:08:15 +0800

    普通高斯消去neon（增加了cache优化版本）
    增加了cache优化

--- SIMD_neon.cpp
+++ SIMD_neon.cpp
@@ -1,3 +1,4 @@
+
 # include <arm_neon.h> // use Neon
 # include <sys/time.h>
 
@@ -6,6 +7,15 @@ using namespace std;
 
 const int n=1000;
 float A[n][n];
+float B[n][n];
+
+float32x4_t va = vmovq_n_f32(0);
+float32x4_t vx = vmovq_n_f32(0);
+float32x4_t vaij = vmovq_n_f32(0);
+float32x4_t vaik = vmovq_n_f32(0);
+float32x4_t vakj = vmovq_n_f32(0);
+
+
 
 void init()
 {
@@ -55,46 +65,200 @@ void f_ordinary()
 }
 
 
+void f_ordinary_cache()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            B[j][i] = A[i][j];
+            A[i][j] = 0; // 相当于原来的 A[i][k] = 0;
+        }
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int j = k + 1; j < n; j++)
+        {
+            A[k][j] = A[k][j] * 1.0 / A[k][k];
+        }
+        A[k][k] = 1.0;
+
+
+
+        for (int i = k + 1; i < n; i++)
+        {
+            for (int j = k + 1; j < n; j++)
+            {
+                A[i][j] = A[i][j] - B[k][i] * A[k][j];
+            }
+            //A[i][k] = 0;
+        }
+    }
+}
+
+
+
+
 void f_pro()
 {
     for (int k = 0; k < n; k++)
 	{
 	    float32x4_t vt=vmovq_n_f32(A[k][k]);
-
 	    int j;
 		for (j = k + 1; j+4 <= n; j+=4)
 		{
-		    float32x4_t va=vld1q_f32(&(A[k][j]) );
-
+		    va=vld1q_f32(&(A[k][j]) );
 			va= vdivq_f32(va,vt);
-
 			vst1q_f32(&(A[k][j]), va);
-
 		}
 
-		for(j = j + 4; j<n; j++)
+		for(; j<n; j++)
         {
             A[k][j]=A[k][j]*1.0 / A[k][k];
 
         }
-
 		A[k][k] = 1.0;
 
 		for (int i = k + 1; i < n; i++)
 		{
-		    float32x4_t vaik=vmovq_n_f32(A[i][k]);
+		    vaik=vmovq_n_f32(A[i][k]);
 
-			for (int j = k + 1; j+4 <= n; j+=4)
+			for (j = k + 1; j+4 <= n; j+=4)
 			{
-				float32x4_t vakj=vld1q_f32(&(A[k][j]));
-				float32x4_t vaij=vld1q_f32(&(A[i][j]));
-				float32x4_t vx=vmulq_f32(vakj,vaik);
+				vakj=vld1q_f32(&(A[k][j]));
+				vaij=vld1q_f32(&(A[i][j]));
+				vx=vmulq_f32(vakj,vaik);
 				vaij=vsubq_f32(vaij,vx);
 
 				vst1q_f32(&A[i][j], vaij);
 			}
 
-			for(j = j + 4; j<n; j++)
+			for(; j<n; j++)
+            {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+
+			A[i][k] = 0;
+		}
+	}
+}
+
+
+void f_pro_cache()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            B[j][i] = A[i][j];
+            A[i][j] = 0; // 相当于原来的 A[i][k] = 0;
+        }
+    }
+
+
+    for (int k = 0; k < n; k++)
+	{
+	    float32x4_t vt=vmovq_n_f32(A[k][k]);
+	    int j;
+		for (j = k + 1; j+4 <= n; j+=4)
+		{
+		    va=vld1q_f32(&(A[k][j]) );
+			va= vdivq_f32(va,vt);
+			vst1q_f32(&(A[k][j]), va);
+		}
+
+		for(; j<n; j++)
+        {
+            A[k][j]=A[k][j]*1.0 / A[k][k];
+
+        }
+		A[k][k] = 1.0;
+
+		for (int i = k + 1; i < n; i++)
+		{
+		    vaik=vmovq_n_f32(B[k][i]);
+
+			for (j = k + 1; j+4 <= n; j+=4)
+			{
+				vakj=vld1q_f32(&(A[k][j]));
+				vaij=vld1q_f32(&(A[i][j]));
+				vx=vmulq_f32(vakj,vaik);
+				vaij=vsubq_f32(vaij,vx);
+
+				vst1q_f32(&A[i][j], vaij);
+			}
+
+			for(; j<n; j++)
+            {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+		}
+	}
+}
+
+
+void f_pro_division()
+{
+    //只优化除法
+    for (int k = 0; k < n; k++)
+	{
+	    float32x4_t vt=vmovq_n_f32(A[k][k]);
+	    int j;
+		for (j = k + 1; j+4 <= n; j+=4)
+		{
+		    va=vld1q_f32(&(A[k][j]) );
+			va= vdivq_f32(va,vt);
+			vst1q_f32(&(A[k][j]), va);
+		}
+
+		for(; j<n; j++)
+        {
+            A[k][j]=A[k][j]*1.0 / A[k][k];
+        }
+		A[k][k] = 1.0;
+
+        //不优化消去
+		for (int i = k + 1; i < n; i++)
+		{
+			for (int j = k + 1; j < n; j++)
+			{
+				A[i][j] = A[i][j] - A[i][k] * A[k][j];
+			}
+			A[i][k] = 0;
+		}
+	}
+}
+
+
+void f_pro_elimination()
+{
+    //不优化除法
+    int j;
+    for (int k = 0; k < n; k++)
+	{
+	    for (j = k + 1; j < n; j++)
+		{
+			A[k][j] = A[k][j] * 1.0 / A[k][k];
+		}
+		A[k][k] = 1.0;
+
+        //只优化消去
+		for (int i = k + 1; i < n; i++)
+		{
+		    vaik=vmovq_n_f32(A[i][k]);
+
+			for (j = k + 1; j+4 <= n; j+=4)
+			{
+				vakj=vld1q_f32(&(A[k][j]));
+				vaij=vld1q_f32(&(A[i][j]));
+				vx=vmulq_f32(vakj,vaik);
+				vaij=vsubq_f32(vaij,vx);
+
+				vst1q_f32(&A[i][j], vaij);
+			}
+
+			for(; j<n; j++)
             {
                 A[i][j] = A[i][j] - A[i][k] * A[k][j];
             }
@@ -123,14 +287,14 @@ void f_pro_alignment()
             va = vdivq_f32(va,vt);
             vst1q_f32(&A[k][j],va);
         }
-        for(;j < maxN; j++)
+        for(;j < n; j++)
         {
             A[k][j] = A[k][j] * 1.0 / A[k][k];
         }
         A[k][k] = 1.0;
-        for(int i = k + 1;i < maxN; i++)
+        for(int i = k + 1;i < n; i++)
         {
-            float32x4_t vaik = vmovq_n_f32(A[i][k]);
+            vaik = vmovq_n_f32(A[i][k]);
             int j = k + 1;
             while((i * n + j) % 4 != 0)
             {
@@ -138,23 +302,20 @@ void f_pro_alignment()
                 A[i][j] = A[i][j] - A[k][j] * A[i][k];
                 j++;
             }
-            for(;j + 4 <= maxN;j += 4){
+            for(;j + 4 <= n;j += 4){
                 vakj = vld1q_f32(&A[k][j]);
                 vaij = vld1q_f32(&A[i][j]);
                 vx = vmulq_f32(vakj,vaik);
                 vaij = vsubq_f32(vaij,vx);
                 vst1q_f32(&A[i][j],vaij);
             }
-            for(;j < maxN; j++){
+            for(;j < n; j++){
                 A[i][j] = A[i][j] - A[k][j] * A[i][k];
             }
             A[i][k] = 0.0;
         }
     }
-
 }
-
-
 
 void getResult()
 {
@@ -172,25 +333,63 @@ void getResult()
 
 int main()
 {
-    init();
+
     struct timeval head,tail;
 
-    gettimeofday(&head, NULL);//开始计时
-    f_ordinary();
-    gettimeofday(&tail, NULL);//结束计时
-
-    double seconds = ((tail.tv_sec - head.tv_sec)*1000000 + (tail.tv_usec - head.tv_usec)) / 1000.0;//单位 ms
-    cout<<"f_ordinary: "<<seconds<<" ms"<<endl;
-
-
-
+    init();
     gettimeofday(&head, NULL);//开始计时
     f_pro();
     gettimeofday(&tail, NULL);//结束计时
     seconds = ((tail.tv_sec - head.tv_sec)*1000000 + (tail.tv_usec - head.tv_usec)) / 1000.0;//单位 ms
     cout<<"f_pro: "<<seconds<<" ms"<<endl;
 
-    //getResult();
+    init();
+    gettimeofday(&head, NULL);//开始计时
+    f_pro_cache();
+    gettimeofday(&tail, NULL);//结束计时
+    seconds = ((tail.tv_sec - head.tv_sec)*1000000 + (tail.tv_usec - head.tv_usec)) / 1000.0;//单位 ms
+    cout<<"f_pro_cache: "<<seconds<<" ms"<<endl;
+
+
+    init();
+    gettimeofday(&head, NULL);//开始计时
+    f_ordinary();
+    gettimeofday(&tail, NULL);//结束计时
+    double seconds = ((tail.tv_sec - head.tv_sec)*1000000 + (tail.tv_usec - head.tv_usec)) / 1000.0;//单位 ms
+    cout<<"f_ordinary: "<<seconds<<" ms"<<endl;
+
+
+    init();
+    gettimeofday(&head, NULL);//开始计时
+    f_ordinary_cache();
+    gettimeofday(&tail, NULL);//结束计时
+    seconds = ((tail.tv_sec - head.tv_sec)*1000000 + (tail.tv_usec - head.tv_usec)) / 1000.0;//单位 ms
+    cout<<"f_ordinary_cache: "<<seconds<<" ms"<<endl;
+
+    init();
+    gettimeofday(&head, NULL);//开始计时
+    f_pro_alignment();
+    gettimeofday(&tail, NULL);//结束计时
+    seconds = ((tail.tv_sec - head.tv_sec)*1000000 + (tail.tv_usec - head.tv_usec)) / 1000.0;//单位 ms
+    cout<<"f_pro_alignment: "<<seconds<<" ms"<<endl;
+
+
+    init();
+    gettimeofday(&head, NULL);//开始计时
+    f_pro_division();
+    gettimeofday(&tail, NULL);//结束计时
+    seconds = ((tail.tv_sec - head.tv_sec)*1000000 + (tail.tv_usec - head.tv_usec)) / 1000.0;//单位 ms
+    cout<<"f_pro_division: "<<seconds<<" ms"<<endl;
+
+
+    init();
+    gettimeofday(&head, NULL);//开始计时
+    f_pro_elimination();
+    gettimeofday(&tail, NULL);//结束计时
+    seconds = ((tail.tv_sec - head.tv_sec)*1000000 + (tail.tv_usec - head.tv_usec)) / 1000.0;//单位 ms
+    cout<<"f_pro_elimination: "<<seconds<<" ms"<<endl;
+
+
 
 }
 
